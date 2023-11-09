09b524b890971540f556d514d1dc71e2fb1c4c84
KauanneSilva <kauannesilv2405@gmail.com>
2023-11-09 08:55:15 -0300

    esporte

---
+++ src/Pessoa.java
@@ -0,0 +1,9 @@
+public class Pessoa {
+    //Atributos
+    private String nome;
+    private String cpf;
+    private String dataNascimento;
+    private LocalDate dataNascimento;
+    private double peso;
+    private double altura;
+}
